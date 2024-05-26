aa2aecc338da6683a5d129d28434fa9ea0f00193
noctisatrae <noctisatrae@pm.me>
2024-05-26 20:05:34 +0200

    rewrite my bad understanding of goroutines

--- relay/msg_handler.go
+++ relay/msg_handler.go
@@ -6,83 +6,77 @@ import (
 	"github.com/charmbracelet/log"
 )
 
-type HandlerBehaviour[T any] func(data *protos.MessageData) (T, error)
+type HandlerBehaviour func(data *protos.MessageData) error
 
-type Handler[T any] struct {
-	CastAddHandler        HandlerBehaviour[T]
-	CastRemoveHandler     HandlerBehaviour[T]
-	FrameActionHandler    HandlerBehaviour[T]
-	ReactionAddHandler    HandlerBehaviour[T]
-	ReactionRemoveHandler HandlerBehaviour[T]
+type Handler struct {
+	CastAddHandler        HandlerBehaviour
+	CastRemoveHandler     HandlerBehaviour
+	FrameActionHandler    HandlerBehaviour
+	ReactionAddHandler    HandlerBehaviour
+	ReactionRemoveHandler HandlerBehaviour
 }
 
 // T: What will be returned when the message is handled.
-func (handler Handler[T]) handleMessages(messagesChan chan *protos.GossipMessage, ll log.Logger) {
-	for messageBundle := range messagesChan {
-		messages := messageBundle.GetMessageBundle().GetMessages()
-		ll.Debug(messages)
-		for m := range messages {
-			data := messages[m].GetData()
+func (handler Handler) handleMessages(messages chan *protos.GossipMessage, ll log.Logger) {
+	for msgB := range messages { // i hope that the message only gives one message at a time so it's just O(n) and not O(n²)
+		for _, m := range msgB.GetMessageBundle().GetMessages() {
+			data := m.Data
 			switch data.Type {
 			case protos.MessageType_MESSAGE_TYPE_CAST_ADD:
 				{
 					if handler.CastAddHandler == nil {
-						ll.Warn("CastAdd event was not handled because no function was provided!")
-						return
+						ll.Info("New cast published! |", "Body", data.GetCastAddBody())
+					} else {
+						err := handler.CastAddHandler(data)
+						if err != nil {
+							ll.Error("CastAdd handler encountered an error! |", "Error", err)
+						}
 					}
-					result, err := handler.CastAddHandler(data)
-					if err != nil {
-						ll.Error("CastAdd handler encountered an error! |", "Error", err)
-					}
-					ll.Info("CastAdd event handled! |", "Result", result)
 				}
 			case protos.MessageType_MESSAGE_TYPE_CAST_REMOVE:
 				{
 					if handler.CastRemoveHandler == nil {
-						ll.Warn("CastRemove event was not handled because no function was provided!")
-						return
+						ll.Info("Cast was just removed! |", "Body", data.GetCastRemoveBody())
+					} else {
+						err := handler.CastRemoveHandler(data)
+						if err != nil {
+							ll.Error("CastRemove handler encountered an error! |", "Error", err)
+						}
 					}
-					result, err := handler.CastRemoveHandler(data)
-					if err != nil {
-						ll.Error("CastRemove handler encountered an error! |", "Error", err)
-					}
-					ll.Info("CastRemove event handled! |", "Result", result)
 				}
 			case protos.MessageType_MESSAGE_TYPE_FRAME_ACTION:
 				{
 					if handler.FrameActionHandler == nil {
-						ll.Warn("FrameAction event was not handled because no function was provided!")
-						return
+						ll.Info("New frame interaction! |", "Action", data.GetFrameActionBody())
+					} else {
+						err := handler.FrameActionHandler(data)
+						if err != nil {
+							ll.Error("FrameAction handler encountered an error! |", "Error", err)
+						}
 					}
-					result, err := handler.FrameActionHandler(data)
-					if err != nil {
-						ll.Error("FrameAction handler encountered an error! |", "Error", err)
-					}
-					ll.Info("FrameAction event handled! |", "Result", result)
 				}
 			case protos.MessageType_MESSAGE_TYPE_REACTION_ADD:
 				{
 					if handler.ReactionAddHandler == nil {
-						ll.Warn("ReactionAdd event was not handled because no function was provided!")
-						return
+						ll.Info("New reaction added! |", "Reaction", data.GetReactionBody())
+					} else {
+						err := handler.ReactionAddHandler(data)
+						if err != nil {
+							ll.Error("ReactionAdd handler encountered an error! |", "Error", err)
+						}
 					}
-					result, err := handler.ReactionAddHandler(data)
-					if err != nil {
-						ll.Error("ReactionAdd handler encountered an error! |", "Error", err)
-					}
-					ll.Info("ReactionAdd event handled! |", "Result", result)
 				}
 			case protos.MessageType_MESSAGE_TYPE_REACTION_REMOVE:
 				{
 					if handler.ReactionRemoveHandler == nil {
-						ll.Warn("ReactionRemove event was not handled because no function was provided!")
+						ll.Info("A reaction was removed! |", "Reaction", data.GetReactionBody())
 						return
+					} else {
+						err := handler.ReactionRemoveHandler(data)
+						if err != nil {
+							ll.Error("ReactionRemove handler encountered an error! |", "Error", err)
+						}
 					}
-					result, err := handler.ReactionRemoveHandler(data)
-					if err != nil {
-						ll.Error("ReactionRemove handler encountered an error! |", "Error", err)
-					}
-					ll.Info("ReactionRemove event handled! |", "Result", result)
 				}
 			default:
 				ll.Warn("Unhandled message type! |", "Type", data.Type)
