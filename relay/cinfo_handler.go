ffb4977f3114375081148842470d8ef31562fc02
noctisatrae <noctisatrae@pm.me>
2024-05-27 00:19:39 +0200

    no panic implementation

--- relay/cinfo_handler.go
+++ relay/cinfo_handler.go
@@ -16,37 +16,40 @@ import (
 // Channel => Message => Content
 func HandleContactInfo(contactInfoChan chan *protos.GossipMessage, ll log.Logger, h host.Host, ctx context.Context) {
 	for contactInfoMessage := range contactInfoChan {
-		cinfo := contactInfoMessage.GetContactInfoContent()
-
-		remotePeerAddrFamily := cinfo.GossipAddress.GetFamily()
-		remotePeerAddr := cinfo.GossipAddress.GetAddress()
-		remotePeerPort := cinfo.GossipAddress.GetPort()
-
-		ll.Info("Received contact info! |", "Addr", remotePeerAddr, "Port", remotePeerPort, "Family", remotePeerAddrFamily)
-
-		remotePeerMultiAddr, err := multiaddr.NewMultiaddr(fmt.Sprintf(
-			"/ip%s/%s/tcp/%s/p2p/12D3KooWEWyEQXVaeUb7rtBmXBW7i4TcwTyS8qwmSdvYTNvnZVnv",
-			strconv.FormatUint(uint64(remotePeerAddrFamily), 10),
-			remotePeerAddr,
-			strconv.FormatUint(uint64(remotePeerPort), 10),
-		))
+		remotePeerId, err := peer.IDFromBytes(contactInfoMessage.GetPeerId())
 		if err != nil {
-			ll.Error("Can't parse multiaddrr from contact info! |", "Error", err)
+			ll.Error("Can't serialize the peer id from message! |", "Error", err)
 		} else {
-			for _, conn := range h.Network().Conns() {
-				if remotePeerMultiAddr == conn.RemoteMultiaddr() {
-					ll.Info("Won't connect to this address as we're already connected")
+			remotePeerIdStr := remotePeerId.String()
+			cinfo := contactInfoMessage.GetContactInfoContent()
+
+			remotePeerAddrFamily := cinfo.GossipAddress.GetFamily()
+			remotePeerAddr := cinfo.GossipAddress.GetAddress()
+			remotePeerPort := cinfo.GossipAddress.GetPort()
+
+			ll.Info("Received contact info! |", "Addr", remotePeerAddr, "Port", remotePeerPort, "Family", remotePeerAddrFamily)
+
+			remotePeerMultiAddr, err := multiaddr.NewMultiaddr(fmt.Sprintf(
+				"/ip%s/%s/tcp/%s/p2p/%s",
+				strconv.FormatUint(uint64(remotePeerAddrFamily), 10),
+				remotePeerAddr,
+				strconv.FormatUint(uint64(remotePeerPort), 10),
+				remotePeerIdStr,
+			))
+			ll.Debug("From contact info! |", "Multiaddr", remotePeerMultiAddr)
+			if err != nil {
+				ll.Error("Can't parse multiaddrr from contact info! |", "Error", err)
+			} else {
+
+				remotePeerAddrInfo, err := peer.AddrInfoFromP2pAddr(remotePeerMultiAddr)
+				if err != nil {
+					ll.Error("Can't create AddrInfo from contact info! |", "Error", err, "Multiaddr", remotePeerMultiAddr)
 				} else {
-					remotePeerAddrInfo, err := peer.AddrInfoFromP2pAddr(remotePeerMultiAddr)
+					err = h.Connect(ctx, *remotePeerAddrInfo)
 					if err != nil {
-						ll.Error("Can't create AddrInfo from contact info! |", "Error", err, "Multiaddr", remotePeerMultiAddr)
+						ll.Error("Couldn't connect to peer from contact info! |", "Error", err)
 					} else {
-						err = h.Connect(ctx, *remotePeerAddrInfo)
-						if err != nil {
-							ll.Error("Couldn't connect to peer from contact info! |", "Error", err)
-						} else {
-							ll.Info("Connected to peer from contact info! |", "Addr", remotePeerAddr)
-						}
+						ll.Info("Connected to peer from contact info! |", "Addr", remotePeerAddr)
 					}
 				}
 			}
