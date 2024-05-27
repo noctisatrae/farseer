68515d2c0241c802406cb1ae12e54d17d3083500
noctisatrae <noctisatrae@pm.me>
2024-05-27 17:40:35 +0200

    add some test for config handling

---
+++ relay/config_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/charmbracelet/log"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLoadConfig(t *testing.T) {
+	log.SetLevel(log.DebugLevel)
+
+	conf, err := Load("./config.toml")
+	assert.NoError(t, err)
+
+	rpcConf := conf.Servers["rpc"]
+	dbConf := conf.Databases["redis"]
+
+	// always-the-same option test
+	assert.Equal(t, HubParams{
+		GossipPort:      2282,
+		BootstrapPeers:  []string{"/dns/nemes.farcaster.xyz/tcp/2282/p2p/12D3KooWMQrf6unpGJfLBmTGy3eKTo4cGcXktWRbgMnfbZLXqBbn"},
+		Debug:           false,
+		BufferSize:      128,
+		ContactInterval: 30,
+	}, conf.Hub)
+
+	// dynamic conf
+	assert.Equal(t, true, rpcConf.(map[string]interface{})["enabled"])
+	assert.Equal(t, true, dbConf.(map[string]interface{})["enabled"])
+}
