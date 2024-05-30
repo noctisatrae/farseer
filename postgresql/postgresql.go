979e8c9c0fe51c4d2ad05a540b42a27c3bdb0a1e
noctisatrae <noctisatrae@pm.me>
2024-05-30 18:32:03 +0200

    initiation of db conn for plugin

--- postgresql/postgresql.go
+++ postgresql/postgresql.go
@@ -1,11 +1,42 @@
 package main
 
 import (
+	"context"
+	"errors"
 	handler "farseer/handlers"
 	protos "farseer/protos"
 
 	"github.com/charmbracelet/log"
+	"github.com/jackc/pgx/v5"
 )
+
+// InitBehaviour initializes the plugin by setting up a database connection.
+//
+// Example config.toml for this plugin:
+//
+// ```toml
+// [handlers.postgresql]
+// DbAddress = "your-database-address" # required
+// ```
+//
+// Parameters:
+// - params: A map containing configuration parameters.
+//
+func InitBehaviour(params map[string]interface{}) error {
+	dbAddr := params["DbAddress"]
+	if dbAddr == nil {
+		return errors.New("no DbAddress was provided, so no connection can be made to the DB")
+	}
+	
+	conn, err := pgx.Connect(context.Background(), dbAddr.(string))
+	if err != nil {
+		return err
+	}
+
+	params["dbConn"] = conn
+
+	return nil
+}
 
 func CastAddHandler(data *protos.MessageData, params map[string]interface{}) error {
 	log.Debug("PLUGIN LOADED!", "Params", params, "Data", data)
@@ -19,7 +50,8 @@ func CastRemoveHandler(data *protos.MessageData, params map[string]interface{}) 
 
 // Exported variable
 var PluginHandler = handler.Handler{
+	Name: "PostgreSQL",
+	InitHandler: InitBehaviour,
 	CastAddHandler:    CastAddHandler,
 	CastRemoveHandler: CastRemoveHandler,
-	// Initialize other handlers as needed...
 }
