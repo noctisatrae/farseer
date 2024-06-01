193bb0ee70742aaf16a5ef595eb82fcf4cadc14b
noctisatrae <noctisatrae@pm.me>
2024-06-01 16:40:22 +0200

    intersection of array of T comparable into farseer/utils

---
+++ utils/intersection_of_T.go
@@ -0,0 +1,28 @@
+package utils
+
+import protos "farseer/protos"
+
+func MsgTypeToInt(msgType protos.MessageType) int64 {
+	return int64(msgType.Number())
+}
+
+func IntersectionOfArrays[T comparable](a []T, b []T) []T {
+	set := make([]T, 0)
+
+	for _, v := range a {
+		if containsGeneric(b, v) {
+			set = append(set, v)
+		}
+	}
+
+	return set
+}
+
+func containsGeneric[T comparable](b []T, e T) bool {
+	for _, v := range b {
+		if v == e {
+			return true
+		}
+	}
+	return false
+}
