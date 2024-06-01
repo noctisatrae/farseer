bbd85a503648c922d6f94cb1db23bba41e978e6b
noctisatrae <noctisatrae@pm.me>
2024-06-01 16:38:49 +0200

    (hard AF) test msg FID + type filter

--- postgresql/postgresql_test.go
+++ postgresql/postgresql_test.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"errors"
 	"farseer/config"
+	protos "farseer/protos"
+	"farseer/time"
 	"testing"
 
+	"github.com/charmbracelet/log"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -21,4 +25,33 @@ func TestParamsFromConf(t *testing.T) {
 
 	err = InitBehaviour(conf.GetParams("postgresql"))
 	assert.NoError(t, err)
+}
+
+func TestParamsCheck(t *testing.T) {
+	log.SetLevel(log.DebugLevel)
+
+	conf, err := config.Load("../config.toml")
+	assert.NoError(t, err)
+
+	params := conf.GetParams("postgresql")
+
+	currentFcTime, err := time.GetFarcasterTime()
+	assert.NoError(t, err)
+
+	msgData := protos.MessageData{
+		Type: protos.MessageType_MESSAGE_TYPE_CAST_ADD,
+		Fid: 2,
+		Timestamp: uint32(currentFcTime),
+		Network: protos.FarcasterNetwork_FARCASTER_NETWORK_MAINNET,
+		Body: &protos.MessageData_CastAddBody{},
+	}
+
+	msgFilter := params["MessageTypesAllowed"]
+	log.Debug(msgFilter, "MsgType", msgData.Type.Number())
+
+	err = CheckConfigParams(&msgData, params, func(data *protos.MessageData, params map[string]interface{}) error {
+		return errors.New("if this error is raised, the test pass")
+	})
+
+	assert.Error(t, err)
 }
