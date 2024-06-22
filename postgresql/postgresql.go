8670e2cbb8bf150bbfa56f51c45e59b54e1923ed
noctisatrae <noctisatrae@pm.me>
2024-06-23 00:47:44 +0200

    castRemove & add handlers

--- postgresql/postgresql.go
+++ postgresql/postgresql.go
@@ -8,6 +8,7 @@ import (
 	protos "farseer/protos"
 	utils "farseer/utils"
 
+	"github.com/charmbracelet/log"
 	"github.com/jackc/pgx/v5"
 )
 
@@ -17,8 +18,9 @@ const (
 		fid, 
 		timestamp, 
 		network, 
-		type, 
-		cast_add_text, 
+		type,
+		hash,
+		cast_add_text,
 		cast_add_parent_cast_id_fid, 
 		cast_add_parent_cast_id_hash, 
 		cast_add_parent_url, 
@@ -26,16 +28,30 @@ const (
 		cast_add_mentions, 
 		cast_add_mentions_positions, 
 		cast_add_embeds
-	) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)
+	) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)
 	`
 
-	sqlCastRemove = `
-	INSERT INTO cast_remove_messages (message_id, target_hash)
-	VALUES ($1, $2)
+	sqlCastAddRemoved = `
+	INSERT INTO all_messages (
+		fid, 
+		timestamp,
+		network,
+		type,
+		hash,
+		removed_at
+	) VALUES ($1, $2, $3, $4, $5, CURRENT_TIMESTAMP)
+	`
+
+	sqlCastUpdateRemoved = `
+UPDATE all_messages
+SET 
+    removed_at = CURRENT_TIMESTAMP
+WHERE 
+    hash = $1;
 	`
 )
 
-func CheckConfigParams(data *protos.MessageData, params map[string]interface{}, handlerFunc handler.HandlerBehaviour) error {
+func CheckConfigParams(data *protos.MessageData, params map[string]interface{}, hash []byte, handlerFunc handler.HandlerBehaviour) error {
 	msgFilter := params["MessageTypesAllowed"]
 	fidFilter := params["FidsAllowed"]
 
@@ -43,22 +59,22 @@ func CheckConfigParams(data *protos.MessageData, params map[string]interface{}, 
 		allowedMsgType := utils.IntersectionOfArrays(msgFilter.([]interface{}), []interface{}{utils.MsgTypeToInt(data.Type)})
 		allowedFids := utils.IntersectionOfArrays(fidFilter.([]interface{}), []interface{}{int64(data.Fid)})
 		if len(allowedMsgType) > 0 && len(allowedFids) > 0 {
-			return handlerFunc(data, params)
+			return handlerFunc(data, hash, params)
 		}
 
 		return nil
 	} else if msgFilter == nil && fidFilter != nil {
 		allowedFids := utils.IntersectionOfArrays(fidFilter.([]interface{}), []interface{}{int64(data.Fid)})
 		if len(allowedFids) > 0 {
-			return handlerFunc(data, params)
+			return handlerFunc(data, hash, params)
 		}
 	} else if msgFilter != nil && fidFilter == nil {
 		allowedMsgType := utils.IntersectionOfArrays(msgFilter.([]interface{}), []interface{}{utils.MsgTypeToInt(data.Type)})
 		if len(allowedMsgType) > 0 {
-			return handlerFunc(data, params)
+			return handlerFunc(data, hash, params)
 		}
 	} else {
-		return handlerFunc(data, params)
+		return handlerFunc(data, hash, params)
 	}
 
 	return nil
@@ -93,20 +109,22 @@ func InitBehaviour(params map[string]interface{}) error {
 	return nil
 }
 
-func CastAddHandler(data *protos.MessageData, params map[string]interface{}) error {
+func CastAddHandler(data *protos.MessageData, hash []byte, params map[string]interface{}) error {
 	hdlCtx := params["hdlCtx"].(context.Context)
 	conn := params["dbConn"].(*pgx.Conn)
 
 	castAddBody := data.GetCastAddBody()
+	log.Debug("CastAddHandler, handling message", "Hash", utils.BytesToHex(hash))
 
 	_, err := conn.Exec(hdlCtx, sqlCastAdd,
 		data.Fid,
 		data.Timestamp,
-		data.Network.String(),
-		data.Type.String(),
+		data.Network.Number(),
+		data.Type.Number(),
+		utils.BytesToHex(hash),	
 		castAddBody.Text,
 		castAddBody.GetParentCastId().GetFid(),
-		castAddBody.GetParentCastId().GetHash(),
+		utils.BytesToHex(castAddBody.GetParentCastId().GetHash()),
 		castAddBody.GetParentUrl(),
 		castAddBody.EmbedsDeprecated,
 		castAddBody.Mentions,
@@ -120,11 +138,44 @@ func CastAddHandler(data *protos.MessageData, params map[string]interface{}) err
 	return nil
 }
 
+func CastRemoveHandler(data *protos.MessageData, hash []byte, params map[string]interface{}) error {
+	hdlCtx := params["hdlCtx"].(context.Context)
+	conn := params["dbConn"].(*pgx.Conn)
+
+	var id int
+	castIdToRemove := utils.BytesToHex(data.GetCastRemoveBody().TargetHash) 
+
+	err := conn.QueryRow(hdlCtx, sqlCastUpdateRemoved, castIdToRemove).Scan(&id)
+	if err == pgx.ErrNoRows {
+		_, err := conn.Exec(
+			hdlCtx, 
+			sqlCastAddRemoved, 
+			// args
+			data.Fid,
+			data.Timestamp,
+			data.Network.Number(),
+			data.Type.Number(),
+			castIdToRemove,
+			data.Timestamp,
+		)
+		if err != nil {
+			return err
+		}
+	} else {
+		return err
+	}
+
+	return nil
+}
+
 // Exported variable
 var PluginHandler = handler.Handler{
 	Name:        "PostgreSQL",
 	InitHandler: InitBehaviour,
-	CastAddHandler: func(data *protos.MessageData, params map[string]interface{}) error {
-		return CheckConfigParams(data, params, CastAddHandler)
+	CastAddHandler: func(data *protos.MessageData, hash []byte, params map[string]interface{}) error {
+		return CheckConfigParams(data, params, hash, CastAddHandler)
+	},
+	CastRemoveHandler: func(data *protos.MessageData, hash []byte, params map[string]interface{}) error {
+		return CheckConfigParams(data, params, hash, CastRemoveHandler)
 	},
 }
