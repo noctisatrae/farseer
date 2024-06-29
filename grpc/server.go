1d4811f553444c76675ca830ed863557cefe34ef
noctisatrae <noctisatrae@pm.me>
2024-06-29 15:11:01 +0200

    rpc basics

---
+++ grpc/server.go
@@ -0,0 +1,65 @@
+package grpc
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"os"
+	"sync"
+
+	protos "farseer/protos"
+
+	"github.com/charmbracelet/log"
+	"google.golang.org/grpc"
+)
+
+const PORT = 2285
+
+
+type hubRPCServer struct {
+	protos.UnimplementedHubServiceServer
+	rpcServer map[string][]*protos.HubServiceServer
+}
+
+func (s *hubRPCServer) SubmitMessage(ctx context.Context, message *protos.Message) (*protos.Message, error) {
+	return message, nil
+}
+
+func (s *hubRPCServer) ValidateMessage(ctx context.Context, message *protos.Message) (*protos.ValidationResponse, error) {
+	return &protos.ValidationResponse{Valid: true}, nil
+}
+
+func newServer() *hubRPCServer {
+	s := &hubRPCServer{
+		rpcServer: make(map[string][]*protos.HubServiceServer),
+	}
+	return s
+}
+
+func Start(wg *sync.WaitGroup, stopCh <-chan struct{}) {
+	defer wg.Done()
+
+	ll := log.New(os.Stderr)
+	ll.SetPrefix("grpc")
+
+	// todo: read from config
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", PORT))
+	if err != nil {
+		ll.Fatal("Can't start the listnener! |", "Err", err)
+	}
+
+	ll.Info("Started the server!")
+
+	grpcServer := grpc.NewServer(grpc.EmptyServerOption{})
+	protos.RegisterHubServiceServer(grpcServer, newServer())
+	go func() {
+		if err := grpcServer.Serve(lis); err != nil {
+			log.Fatal("Failed to serve:", err)
+		}	
+	}()
+
+	<- stopCh
+
+	grpcServer.GracefulStop()
+	ll.Info("Graceful shutdown was successful!")
+}
---
+++ grpc/server_test.go
@@ -0,0 +1,23 @@
+package grpc
+
+import (
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestGracefulShutdown(t *testing.T) {
+	var wg sync.WaitGroup
+	stopCh := make(chan struct{})
+
+	// Start the gRPC server in a separate goroutine
+	wg.Add(1)
+	go Start(&wg, stopCh)
+
+	time.Sleep(time.Second)
+
+	close(stopCh)
+
+	// Wait for the server to stop
+	wg.Wait()
+}
