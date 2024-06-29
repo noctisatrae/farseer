d0f77117fac40d5346c14981fc6987b9659a6eb5
noctisatrae <noctisatrae@pm.me>
2024-06-29 18:43:56 +0200

    implement SubmitMessage

--- relay/server.go
+++ relay/server.go
@@ -8,6 +8,7 @@ import (
 	"sync"
 
 	protos "farseer/protos"
+	"farseer/time"
 
 	"github.com/charmbracelet/log"
 	"google.golang.org/grpc"
@@ -18,26 +19,55 @@ const PORT = 2285
 
 
 type hubRPCServer struct {
+	// utils
+	netw Network
+	ll log.Logger
+
 	protos.UnimplementedHubServiceServer
 	rpcServer map[string][]*protos.HubServiceServer
 }
 
-func (s *hubRPCServer) SubmitMessage(ctx context.Context, message *protos.Message) (*protos.Message, error) {
-	return message, nil
+func (s *hubRPCServer) SubmitMessage(ctx context.Context, message *protos.Message) (*protos.Message, error) {	
+	peerIdEncoded, err := s.netw.self.Marshal() 
+	if err != nil {
+		return &protos.Message{}, err
+	}
+
+	contactInfoTime, err := time.GetFarcasterTime()
+	if err != nil {
+		contactInfoTime = 0
+		s.netw.logger.Error("Couldn't get Farcaster time for the message!")
+	}
+
+	msg := protos.GossipMessage{
+		Content: &protos.GossipMessage_Message{
+			Message: message,
+		},
+		Topics: []string{s.netw.topic.String()},
+		PeerId: peerIdEncoded,
+		Version: protos.GossipVersion_GOSSIP_VERSION_V1_1,
+		Timestamp: uint32(contactInfoTime),
+	}
+
+	s.netw.Publish(&msg)
+
+	return message, nil 
 }
 
 func (s *hubRPCServer) ValidateMessage(ctx context.Context, message *protos.Message) (*protos.ValidationResponse, error) {
 	return &protos.ValidationResponse{Valid: true}, nil
 }
 
-func newServer() *hubRPCServer {
+func newServer(netw Network, ll log.Logger) *hubRPCServer {
 	s := &hubRPCServer{
+		netw: netw,
+		ll: ll,
 		rpcServer: make(map[string][]*protos.HubServiceServer),
 	}
 	return s
 }
 
-func Start(wg *sync.WaitGroup, stopCh <-chan struct{}) {
+func Start(wg *sync.WaitGroup, stopCh <-chan struct{}, netw Network) {
 	defer wg.Done()
 
 	ll := log.New(os.Stderr)
@@ -52,7 +82,7 @@ func Start(wg *sync.WaitGroup, stopCh <-chan struct{}) {
 	ll.Info("Started the server!")
 
 	grpcServer := grpc.NewServer(grpc.EmptyServerOption{})
-	protos.RegisterHubServiceServer(grpcServer, newServer())
+	protos.RegisterHubServiceServer(grpcServer, newServer(netw, *ll))
 	reflection.Register(grpcServer)
 	go func() {
 		if err := grpcServer.Serve(lis); err != nil {
