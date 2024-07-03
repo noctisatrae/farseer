29aa9fed8d4e296fb3e931f4ef2921d01345e697
noctisatrae <noctisatrae@pm.me>
2024-07-03 13:35:03 +0200

    register signer

--- ts_testing/index.ts
+++ ts_testing/index.ts
@@ -1,14 +1,42 @@
-import { CastType, EthersEip712Signer, FarcasterNetwork, NobleEd25519Signer, getFarcasterTime, getInsecureHubRpcClient, makeCastAdd } from "@farcaster/hub-nodejs";
-import { Wallet } from "ethers";
+import { CastType, FarcasterNetwork, KEY_GATEWAY_ADDRESS, ViemLocalEip712Signer, getFarcasterTime, getInsecureHubRpcClient, keyGatewayABI, makeCastAdd } from "@farcaster/hub-nodejs";
+import { createWalletClient, fromBytes, http, publicActions, toHex } from "viem";
+import { mnemonicToAccount, toAccount } from "viem/accounts";
+import { optimism } from "viem/chains";
 
 const HUB_URL = "38.242.131.38:2283" 
 const MNEMONIC = Bun.env.MNEMONIC ?? ""
-const NETWORK = FarcasterNetwork.MAINNET
+const OP_ENDPOINT = Bun.env.OP_ENDPOINT;
 const FID = 10626
+const NETWORK = FarcasterNetwork.MAINNET
 
-const wallet = Wallet.fromPhrase(MNEMONIC)
-const signer = new EthersEip712Signer(wallet)
-const clt = getInsecureHubRpcClient(HUB_URL);
+const KeyContract = { abi: keyGatewayABI, address: KEY_GATEWAY_ADDRESS, chain: optimism };
+const clt = getInsecureHubRpcClient(HUB_URL)
+const account = mnemonicToAccount(MNEMONIC)
+const localAccount = toAccount(account)
+const signer = new ViemLocalEip712Signer(localAccount)
+
+const walletClient = createWalletClient({
+  account,
+  chain: optimism,
+  transport: http(OP_ENDPOINT!),
+}).extend(publicActions);
+
+const metadata = (await signer.getSignedKeyRequestMetadata({
+  requestFid: BigInt(FID),
+  key: account.getHdKey().privateKey!,
+  deadline: BigInt(Math.floor(Date.now() / 1000) + 60 * 60),
+}))._unsafeUnwrap()
+
+const { request: signerAddRequest } = await walletClient.simulateContract({
+  ...KeyContract,
+  functionName: 'add',
+  args: [1, toHex(account.getHdKey().publicKey!), 1, toHex(metadata)], // keyType, publicKey, metadataType, metadata
+});
+
+const signerAddTxHash = await walletClient.writeContract(signerAddRequest);
+console.log(`Waiting for signer add tx to confirm: ${signerAddTxHash}`);
+await walletClient.waitForTransactionReceipt({ hash: signerAddTxHash });
+console.log("Sleeping 30 seconds to allow hubs to pick up the signer tx");
 
 const castToSend = await makeCastAdd(
   {
