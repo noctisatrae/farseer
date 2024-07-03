b77baf495a1862314f6702406cc84b379d3e5295
noctisatrae <noctisatrae@pm.me>
2024-07-03 16:19:24 +0200

    test have change: we have a new default config

--- config/config_test.go
+++ config/config_test.go
@@ -21,16 +21,15 @@ func TestLoadConfig(t *testing.T) {
 	assert.Equal(t, config.HubParams{
 		PublicHubIp: "92.158.95.48",
 		GossipPort:  2282,
-		BootstrapPeers: []string{
-			"/dns/nemes.farcaster.xyz/tcp/2283/p2p/12D3KooWMQrf6unpGJfLBmTGy3eKTo4cGcXktWRbgMnfbZLXqBbn",
-		},
+		RpcPort: 2283,
+		BootstrapPeers: []string{},
 		Debug:           false,
 		BufferSize:      128,
-		ContactInterval: 30,
+		ContactInterval: 3000,
 	}, conf.Hub)
 
 	// dynamic conf
-	assert.Equal(t, true, postgreConf.(map[string]interface{})["Enabled"])
+	assert.Equal(t, false, postgreConf.(map[string]interface{})["Enabled"])
 }
 
 // can we get the params from a handler's configuration?
@@ -38,5 +37,5 @@ func TestParamsFromConf(t *testing.T) {
 	conf, err := config.Load("../config.toml")
 	assert.NoError(t, err)
 
-	assert.Equal(t, map[string]interface{}{"DbAddress": "postgres://postgres:example@localhost:5432/postgres", "MessageTypesAllowed": []interface{}{int64(1), int64(2)}, "FidsAllowed": []interface{}{int64(10626)}}, conf.GetParams("postgresql"))
+	assert.Equal(t, map[string]interface{}{"DbAddress": "postgres://postgres:example@db:5432/postgres", "FidsAllowed": []interface{}{int64(10626)}}, conf.GetParams("postgresql"))
 }
